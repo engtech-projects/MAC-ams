585eb57fa08634e2c760878fc5cc888036231459
Ryan Pastoriza <ryanpastoriza@gmail.com>
2023-09-28 16:23:25 +0800

    Update Accounts.php
    fetch accounts with L type

--- app/Models/Accounts.php
+++ app/Models/Accounts.php
@@ -87,7 +87,7 @@ class Accounts extends Model
                     ->join('account_type', 'account_type.account_type_id', '=', 'chart_of_accounts.account_type_id')
                     ->join('account_category', 'account_category.account_category_id', '=', 'account_type.account_category_id')
                     ->select('chart_of_accounts.*', 'account_type.account_type', 'account_type.*', 'account_category.account_category')
-                    ->where('status', 'active')
+                    ->where(['status' => 'active', 'type' => 'L'])
                     ->orderBy('account_category.account_category_id', 'asc')
                     ->orderBy('account_type.account_type_id', 'asc')
                     ->get();
