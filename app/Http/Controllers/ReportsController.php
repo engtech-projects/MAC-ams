50cc00d7d916b1794dbef9136227cf13b95ca503
Genesis John Abobo <geiizanityy@gmail.com>
2023-11-21 17:00:38 +0800

    Update ReportsController.php

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -413,7 +413,7 @@ class ReportsController extends MainController
             'branches' => Branch::fetchBranch(),
             'account_officers' => AccountOfficer::fetchAccountOfficer(),
         ];
-        return response()->json(['data' => CollectionBreakdown::getCollectionBreakdownByBranch($branchId)]);
+        /* return response()->json(['data' => CollectionBreakdown::getCollectionBreakdownByBranch($branchId)]); */
         return view('reports.sections.cashTransactionBlotter', $data);
     }
 
