0b7beaa99dd0368671a610d75826237f4799e1be
Genesis John Abobo <geiizanityy@gmail.com>
2023-12-05 13:09:31 +0800

    Remove Head Office Branch in dropdown

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -11,6 +11,7 @@ class Branch extends Model
 
     protected $table = 'branch';
     protected $primaryKey = 'branch_id';
+    const BRANCH_HEAD_OFFICE = "head office";
 
     public function userBranch()
     {
@@ -19,7 +20,7 @@ class Branch extends Model
 
     public static function fetchBranch()
     {
-        return Branch::all();
+        return Branch::where('branch_code','!=',00000)->orWhere('branch_name','!=',Branch::BRANCH_HEAD_OFFICE)->get();
     }
 
 }
