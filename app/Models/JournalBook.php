98041093248e18cad4e8591996b8b6a793418aa6
Rizaly Beniga <78832093+benigarizaly@users.noreply.github.com>
2024-04-18 13:01:29 +0800

    Cash Blotter - CRB book in cash received section
    Add 'Cash Received Book' in Cash Received report section

--- app/Models/JournalBook.php
+++ app/Models/JournalBook.php
@@ -18,7 +18,7 @@ class JournalBook extends Model
     const COLLECTION_DEPOSITS_BOOK = 7;
     CONST INTER_BRANCH_BOOKS = [9,4];
     const CASH_RECEIPT_BOOK_BOOK = 4;
-    const CASH_RECEIVED_BOOKS = [1, 9];
+    const CASH_RECEIVED_BOOKS = [1, 9,4];
     const BOOK_CREDIT = 'credit';
     const BOOK_DEBIT = 'debit';
 
