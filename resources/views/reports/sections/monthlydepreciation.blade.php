88ac6e66793b4f4b4f0adc33413ec483e89caa07
Giejie <geiizanityy@gmail.com>
2024-10-03 12:06:38 +0800

    Update monthlydepreciation.blade.php

--- resources/views/reports/sections/monthlydepreciation.blade.php
+++ resources/views/reports/sections/monthlydepreciation.blade.php
@@ -384,12 +384,12 @@
                     <div class="modal-body">
                         <form>
                             <div class="form-group">
-                                <label for="recipient-name" class="col-form-label">Recipient:</label>
-                                <input type="text" class="form-control" id="recipient-name">
+                                <label for="recipient-name" class="col-form-label">Subsidiary Name</label>
+                                <input type="text" class="form-control" id="subsidiary_name">
                             </div>
                             <div class="form-group">
-                                <label for="message-text" class="col-form-label">Message:</label>
-                                <textarea class="form-control" id="message-text"></textarea>
+                                <label for="message-text" class="col-form-label">Subsidiary Code:</label>
+                                <input type="text" class="form-control" id="subsidiary_code">
                             </div>
                         </form>
                     </div>
