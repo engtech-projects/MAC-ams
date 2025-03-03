36f4ae19527edffc2e7e95d5a8835fc1ec9e2cfb
Rizaly Beniga <benigarizaly@gmail.com>
2025-03-03 12:10:19 +0800

    Update monthlydepreciation.blade.php

--- resources/views/reports/sections/monthlydepreciation.blade.php
+++ resources/views/reports/sections/monthlydepreciation.blade.php
@@ -316,14 +316,15 @@
                             <div class="form-group">
                                 <div class="row">
                                     <div class="col-md-6">
-                                        <label for="message-text" class="col-form-label">Salvage:</label>
-                                        <input type="text" v-model="ratePercentage" class="form-control">
+                                        <label for="message-text" class="col-form-label">Rate Percentage(%):</label>
+                                        <input type="number" v-model="subsidiary.sub_salvage" class="form-control"
+                                        id="sub_salvage" required>
                                     </div>
                                     <div class="col-md-6">
-                                        <label for="message-text" class="col-form-label">Rate Percentage(%)::</label>
+                                        <label for="message-text" class="col-form-label">Salvage:</label>
                                         
-                                        <input type="number" v-model="subsidiary.sub_salvage" class="form-control"
-                                            id="sub_salvage" required>
+                                      
+                                            <input type="text" v-model="ratePercentage" class="form-control">
                                     </div>
                                 </div>
 
