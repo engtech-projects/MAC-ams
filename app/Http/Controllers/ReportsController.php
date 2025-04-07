42f1c5211d7a7b11782f02c4db3b6ef3bc57ebfe
Giejie <geiizanityy@gmail.com>
2025-04-07 11:50:25 +0800

    Update ReportsController.php

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -227,7 +227,7 @@ class ReportsController extends MainController
             $subs['monthly_amort'] = $value->monthly_amort;
             $subs['salvage'] = $value->salvage;
             $subs['expensed'] = $value->expensed;
-            $totalPayments = $value->prepaid_expense->prepaid_expense_payments->sum('amount');
+            $totalPayments = $value->prepaid_expense?->prepaid_expense_payments->sum('amount');
             $subs['unexpensed'] =  $value->prepaid_expense ? max(0, $value->unexpensed - $totalPayments) : $value->unexpensed;
             $subs['prepaid_expense'] = $value->prepaid_expense ? $value->prepaid_expense->amount : 0;
 
