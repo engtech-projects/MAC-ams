b31b7248f8aa0d6d9b1018790ab69b69124e02bd
Giejie <geiizanityy@gmail.com>
2025-05-21 15:35:49 +0800

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,7 @@ require('axios');
 
 require('./bootstrap');
 
-import flatpickr from 'flatpickr'
-import 'flatpickr/dist/flatpickr.min.css'
+
 axios.axiosdefaults.baseURL = 'http://localhost/MAC-ams'
 
 
