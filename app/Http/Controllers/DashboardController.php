88fa3c282f919ee6bd015e1b1892ca0d4674ebd2
Giejie <geiizanityy@gmail.com>
2025-07-16 19:42:35 +0800

    Revert "Merge branch 'Monthly-Depreciation-Update' into MAC-207"
    This reverts commit 4499e8604ed8b844dc66999f81110622c4fadb8c, reversing
    changes made to 7afd4a0f7ce62958f1cfed3b9ca52fe9ef67439f.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,47 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Subsidiary;
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends MainController
 {
 
-    public function index()
-    {
+    public function index() {
+    	
+    	
+    	$title = 'MAC-AMS | Dashboard';
 
-
-        $title = 'MAC-AMS | Dashboard';
-
-
-
-        /* $subsidiaries = Subsidiary::where('monthly_due', '>', 0)->whereHas('depreciation_payments', function ($query) {
-            $query->whereDate('date_paid', '>', '2024-04-30');
-        })->get();
-
-        $subsidiaries->map(function ($subsidiary) {
-            $used = $subsidiary->sub_no_amort;
-            $number_of_paid = $subsidiary->depreciation_payments->count();
-            $used = $used - $number_of_paid;
-            $payments = $subsidiary->depreciation_payments;
-            $depreciation_payments = [];
-            foreach ($payments as $payment) {
-                $amount = $used != 0 ? $payment->amount / $used : 0;
-                foreach (range(1, $used) as $i) {
-                    $depreciation_payments[] = [
-                        'sub_id' => $subsidiary->sub_id,
-                        'amount' => round($amount, 2),
-                        'date_paid' => now(),
-                    ];
-                }
-                $subsidiary->depreciation_payments()->createMany($depreciation_payments);
-                $payment->delete();
-            }
-        }); */
-        return view('dashboard.dashboard')->with(compact('title'));
+    	return view('dashboard.dashboard')->with(compact('title'));
     }
 }
