91a1b374a20987263c00cc75f3a4e074c35d79d7
Giejie <geiizanityy@gmail.com>
2025-08-07 13:14:36 +0800

    Update ReportsController.php

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -512,7 +512,6 @@ class ReportsController extends MainController
         if ($subCategory === SubsidiaryCategory::INSUR_ADD) {
             $subId = $branch->branch_code === Branch::BRANCH_CODE_HEAD_OFFICE ? Branch::BRANCH_HEAD_OFFICE_ID : $request->branch_id;
         }
-        dd($subId);
         $accountName = Accounts::where('account_number', $accountNumber)->pluck('account_name')->first();
         foreach ($subsidiaryCategory->accounts as $account) {
             $details = [
